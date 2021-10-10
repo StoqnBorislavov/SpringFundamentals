4a0ab9bf8e943e27cb0ed06505e18a59b4aeefc3
StoqnBorislavov <sbstoyanov27@gmail.com>
2021-10-10 17:37:43 +0300

    Added info files.

---
+++ src/main/java/softuni/mobile/model/binding/package-info.java
@@ -0,0 +1,8 @@
+package softuni.mobile.model.binding;
+
+/**
+ * This package contains binding models.
+ *
+ * We use them in the controllers in order to read
+ * the parameters that come from the client (the browser).
+ */
